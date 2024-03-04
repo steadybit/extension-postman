ead37a66a92f8390079c371b140a5f1c6fd1cfa5
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2024-03-04 18:04:27 +0100

    chore: update dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v2.0.2
+
+- update dependencies
+
 ## v2.0.1
 
 - update dependencies
--- config/specification.go
+++ config/specification.go
@@ -4,7 +4,7 @@
 package config
 
 type Specification struct {
-	PostmanBaseUrl string `json:"postmanBaseUrl" split_words:"true" required:"false" default:"https://api.getpostman.com"`
+	PostmanBaseUrl                     string `json:"postmanBaseUrl" split_words:"true" required:"false" default:"https://api.getpostman.com"`
 	PostmanApiKey                      string `json:"postmanApiKey" split_words:"true" required:"true"`
 	PostmanCollectionDiscoveryInterval string `json:"postmanCollectionDiscoveryInterval" split_words:"true" required:"false" default:"3h"`
 }
