69f9ba189aea902b23434be951496c4ecc4c663d
Johannes Edmeier <johannes.edmeier@steadybit.com>
2026-03-13 23:40:37 +0100

    feat: support if-none-match for the extension list endpoint
    Use a startup timestamp as ETag for the "/" index handler so that
    clients can avoid re-fetching the unchanged extension list.

--- main.go
+++ main.go
@@ -4,6 +4,8 @@
 package main
 
 import (
+	"time"
+
 	_ "github.com/KimMachineGun/automemlimit" // By default, it sets `GOMEMLIMIT` to 90% of cgroup's memory limit.
 	"github.com/rs/zerolog"
 	"github.com/steadybit/action-kit/go/action_kit_api/v2"
@@ -20,6 +22,8 @@ import (
 	"github.com/steadybit/extension-postman/v2/extpostman"
 )
 
+var startedAt = time.Now().Format(time.RFC3339)
+
 func main() {
 	config.ParseConfiguration()
 
@@ -33,7 +37,7 @@ func main() {
 	action_kit_sdk.RegisterAction(extpostman.NewPostmanAction())
 	extsignals.ActivateSignalHandlers()
 
-	exthttp.RegisterHttpHandler("/", exthttp.GetterAsHandler(getExtensionList))
+	exthttp.RegisterHttpHandler("/", exthttp.IfNoneMatchHandler(func() string { return startedAt }, exthttp.GetterAsHandler(getExtensionList)))
 	exthttp.Listen(exthttp.ListenOpts{
 		Port: 8086,
 	})
